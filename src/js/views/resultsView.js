323d7460af351d92117a958002a8191ed4da4383
Srivelavarthipati1 <76962373+Srivelavarthipati1@users.noreply.github.com>
2023-08-28 15:47:56 +0530

    Update resultsView.js

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,22 +1,15 @@
-import View from "./view";
-import previewView from "./previewView";
+import View from './View.js';
+import previewView from './previewView.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
 
-class resultsView extends View {
-    _parentElement = document.querySelector('.results');
-    _errorMessage = `No Recipe Found For Your Query!. Please Try Again!!!`;
-    _message = '';
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again ;)';
+  _message = '';
 
-    _generateMarkup(){
-        //console.log(this._data);
-        return this._data.map(this._generateMarkupPreview).join('');
-       
-    }
-    _generateMarkup(){
-      //console.log(this._data);
-      return this._data
-      .map(result => previewView.render(result,false))
-      .join('');
-     
+  _generateMarkup() {
+    return this._data.map(result => previewView.render(result, false)).join('');
   }
 }
-export default new resultsView();
+
+export default new ResultsView();
